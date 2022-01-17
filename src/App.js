9db3f9d1ef391b68cff369ac9abe53c4504f73c0
Евгений-123 <up.krd2018@gmail.com>
2022-01-17 08:40:44 +0300

    url  project

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Recipe from "./pages/Recipe";
 function App() {
     return (
         <>
-            <Router>
+            <Router basename="/React-food">
                 <Header />
                 <main className="container content">
                     <Switch>
